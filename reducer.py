4a3de24dd47e8aab764657c6298f5e7f524aee60
Alex Weirth <123027477+aweirth@users.noreply.github.com>
2024-06-09 14:04:16 -0700

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,26 +1,33 @@
-#!/usr/bin/python3
-# -*-coding:utf-8 -*
-  
-from operator import itemgetter 
-import sys 
-  
+#!/usr/bin/env python3
+import sys
+
 current_word = None
 current_count = 0
 word = None
 
-for line in sys.stdin: 
-    line = line.strip() 
-    word, count = line.split('\t', 1) 
-    try: 
-        count = int(count) 
-    except ValueError: 
+# Read input line by line
+for line in sys.stdin:
+    # Remove leading and trailing whitespace
+    line = line.strip()
+    # Parse the input we got from mapper.py
+    word, count = line.split('\t', 1)
+    # Convert count (currently a string) to int
+    try:
+        count = int(count)
+    except ValueError:
+        # Count was not a number, so silently ignore/discard this line
         continue
-    if current_word == word: 
-        current_count += count 
-    else: 
-        if current_word: 
-            print('%s\t%s' % (current_word, current_count))
-        current_count = count 
-        current_word = word 
-if current_word == word: 
-    print('%s\t%s' % (current_word, current_count))
+    # This IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            # Write result to standard output
+            print(f'{current_word}\t{current_count}')
+        current_count = count
+        current_word = word
+
+# Do not forget to output the last word if needed!
+if current_word == word:
+    print(f'{current_word}\t{current_count}')
