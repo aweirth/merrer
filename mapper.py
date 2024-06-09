23b5af7f8b653b3dfb2571528e636c24db9f5b93
Alex Weirth <123027477+aweirth@users.noreply.github.com>
2024-06-09 14:20:28 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -4,10 +4,8 @@ import re
 
 # Read input line by line
 for line in sys.stdin:
-    # Remove leading and trailing whitespace
     line = line.strip()
-    # Extract words using a regular expression
     words = re.findall(r'\b\w+\b', line)
-    # Emit the word and a count of 1
+    # count the word
     for word in words:
         print(f'{word.lower()}\t1')
