cb495940334615c0b31b833c30e4ab90abf5d80d
Alex Weirth <123027477+aweirth@users.noreply.github.com>
2024-06-10 08:03:16 -0700

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,26 +1,26 @@
-#!/usr/bin/env python3
-import sys
-
+#!/usr/bin/python3
+# -*-coding:utf-8 -*
+  
+from operator import itemgetter 
+import sys 
+  
 current_word = None
 current_count = 0
 word = None
 
-# line by line input
-for line in sys.stdin:
-    line = line.strip()
-    word, count = line.split('\t', 1)
-    try:
-        count = int(count)
-    except ValueError:
+for line in sys.stdin: 
+    line = line.strip() 
+    word, count = line.split('\t', 1) 
+    try: 
+        count = int(count) 
+    except ValueError: 
         continue
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            print(f'{current_word}\t{current_count}')
-        current_count = count
-        current_word = word
-
-# outputting last word
-if current_word == word:
-    print(f'{current_word}\t{current_count}')
+    if current_word == word: 
+        current_count += count 
+    else: 
+        if current_word: 
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count 
+        current_word = word 
+if current_word == word: 
+    print('%s\t%s' % (current_word, current_count))
